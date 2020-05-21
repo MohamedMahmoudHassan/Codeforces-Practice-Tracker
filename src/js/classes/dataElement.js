fcc422259758218290238a144733ca70bd240a17
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-22 00:50:52 +0200

    Add value to data element as span

--- src/js/classes/dataElement.js
+++ src/js/classes/dataElement.js
@@ -2,6 +2,10 @@ class DataElement {
   constructor(parentEl, title) {
     this.title = $("<li>").text(title + ": ");
     this.title.addClass("dataElement");
+
+    this.value = $("<span>").text("_");
+    this.title.append(this.value);
+
     parentEl.append(this.title);
   }
 }
--- src/js/classes/dataList.js
+++ src/js/classes/dataList.js
@@ -1,7 +1,11 @@
 class DataList {
   constructor(parentEl) {
     this.container = $("<ul>");
-    new DataElement(this.container, "Problems solved");
+    new DataElement(this.container, "Problems Solved");
+    new DataElement(this.container, "Max Rate Problem");
+    new DataElement(this.container, "Problems Rating Sum");
+    new DataElement(this.container, "contests");
+    new DataElement(this.container, "Virtuals");
     parentEl.append(this.container);
   }
 }
