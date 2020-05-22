cded05922808c7a90c3478b561ea28dccc1cadea
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-22 08:38:29 +0200

    Add working add section button

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   const page = $("#pageContent");
   page.empty();
+
   const sections = [new Section(page, 0), new Section(page, 1)];
-  sections.forEach(section => window.setTimeout(section.SubmissionsChart.populate(), 2000));
+  sections.forEach(section => section.SubmissionsChart.populate());
+
+  const addSectionBtn = $("<button>").text("Show previous phase");
+  page.append(addSectionBtn);
+  addSectionBtn.click(() => {
+    const section = new Section(page, sections.length);
+    sections.push(section);
+    section.SubmissionsChart.populate();
+    page.append(addSectionBtn);
+  });
 });
