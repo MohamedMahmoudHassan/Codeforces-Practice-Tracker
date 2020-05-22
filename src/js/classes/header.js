a2213bb19b1d898680f643fbc6ac8cd275766a4e
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-22 07:56:32 +0200

    Add static date to section header

--- src/js/classes/header.js
+++ src/js/classes/header.js
@@ -1,9 +1,15 @@
 class Header {
   constructor(parentEl, weekIndex) {
+    this.wrapper = $("<div>");
+
     this.title = $("<h2>");
     if (!weekIndex) this.title.text("Current week");
     else this.title.text(`Previous week ${weekIndex}`);
 
-    parentEl.append(this.title);
+    this.date = $("<p>").text("15 May 2020 - 22 May 2020");
+    this.date.addClass("headerDate");
+
+    this.wrapper.append(this.title, this.date);
+    parentEl.append(this.wrapper);
   }
 }
