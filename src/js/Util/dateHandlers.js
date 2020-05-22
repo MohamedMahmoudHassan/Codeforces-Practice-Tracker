56f96465a2dd66a6f53c1da4e86722d04e332bf5
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-22 17:30:24 +0200

    Show phase start/end dates of each section

---
+++ src/js/Util/dateHandlers.js
@@ -0,0 +1,41 @@
+const getPreviousDateObject = daysNumber => {
+  const currentDate = getDateObject(new Date());
+  const resultDate = new Date(currentDate.timeStamp - daysNumber * 24 * 60 * 60 * 1000);
+  return getDateObject(resultDate);
+};
+
+const getPhaseStartDate = phaseIndex => {
+  return getPreviousDateObject(6 + phaseIndex * 7);
+};
+
+const getPhaseEndDate = phaseIndex => {
+  return getPreviousDateObject(phaseIndex * 7);
+};
+
+const getMonthName = monthIndex => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  return months[monthIndex];
+};
+
+const getDateObject = date => {
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const dateString = `${day} ${getMonthName(month)} ${year}`;
+  const timeStamp = new Date(dateString).getTime();
+
+  return { day, month, year, dateString, timeStamp };
+};
--- src/js/classes/header.js
+++ src/js/classes/header.js
@@ -6,7 +6,12 @@ class Header {
     if (!phaseIndex) this.title.text("Current phase");
     else this.title.text(`Previous phase #${phaseIndex}`);
 
-    this.date = $("<p>").text("15 May 2020 - 22 May 2020");
+    this.phaseStartDate = getPhaseStartDate(phaseIndex);
+    this.phaseEndDate = getPhaseEndDate(phaseIndex);
+
+    this.date = $("<p>").text(
+      `${this.phaseStartDate.dateString} - ${this.phaseEndDate.dateString}`
+    );
     this.date.addClass("headerDate");
 
     this.wrapper.append(this.title, this.date);
