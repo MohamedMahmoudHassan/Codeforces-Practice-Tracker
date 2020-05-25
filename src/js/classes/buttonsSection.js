f644a9136240f19735fc2c22213f82445f49cd5f
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-05-25 04:50:50 +0200

    Extract add section button

---
+++ src/js/classes/buttonsSection.js
@@ -0,0 +1,18 @@
+class ButtonsSection {
+  constructor(parentEl, sections, apiData) {
+    this.wrapper = $("<div>");
+    this.addSectionBtn = $("<button>").text("Show previous phase");
+
+    this.wrapper.append(this.addSectionBtn);
+    parentEl.append(this.wrapper);
+
+    this.addSectionBtn.click(() => this.addSection(parentEl, sections, apiData));
+  }
+
+  addSection = (page, sections, apiData) => {
+    const section = new Section(page, sections.length, sections[sections.length - 1]);
+    sections.push(section);
+    section.populate(apiData);
+    page.append(this.wrapper);
+  };
+}
--- src/js/loadStaticsTab.js
+++ src/js/loadStaticsTab.js
@@ -8,12 +8,5 @@ const loadStaticsTab = apiData => {
   sections.push(new Section(page, 1, sections[0]));
   sections.forEach(section => section.populate(apiData));
 
-  const addSectionBtn = $("<button>").text("Show previous phase");
-  page.append(addSectionBtn);
-  addSectionBtn.click(() => {
-    const section = new Section(page, sections.length, sections[sections.length - 1]);
-    sections.push(section);
-    section.populate(apiData);
-    page.append(addSectionBtn);
-  });
+  new ButtonsSection(page, sections, apiData);
 };
